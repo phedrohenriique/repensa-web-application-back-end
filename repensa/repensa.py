f781ff288f7bcbd4669a97a68093a9bbc07655f5
Pedro Henrique <phd19994@gmail.com>
2022-05-11 10:51:51 -0300

    comments added

--- repensa/repensa.py
+++ repensa/repensa.py
@@ -3,10 +3,11 @@ import sanic as sn
 import routes as rt
 
 ## instantiated Sanic Server
-
 ## can start server with sanic repensa.app or python repensa.py (with app.run method)
 
 app = sn.Sanic('repensa')
 app.blueprint(rt.routes)
+
+## app object will be running the routes = sn.Blueprint.group() where all the routes will be stated
 
 app.run(host='0.0.0.0', port=8800, debug=True)
--- repensa/routes/__init__.py
+++ repensa/routes/__init__.py
@@ -1,6 +1,12 @@
 import sanic as sn
 from .server import server
 
+## if used the statement $ import server will reference the server.py file while
+## $ from .server import server will import the variable inside the server.py file
+
+## file where to put all route Blueprint objects, for organization each route will have its
+## own handler to be dealt and url
+
 routes = sn.Blueprint.group(
     server, 
     url_prefix='/'
